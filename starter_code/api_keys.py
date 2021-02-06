eb53670c009292f8be5dd635d29b6d6d5690752b
catherinedhwang <catherine.d.hwang@gmail.com>
2021-02-06 09:33:03 -0500

    final push

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9dffe9723f59ed416ee66df36c914eac"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBOyAgiraJBWSCnihNMX08MDlJ9KmfK4J4"
